02b9ff6357f6b4f25b016e6b9f789204240c3af0
Johanna Geßner <johanna-gessner@web.de>
2023-09-01 17:11:41 +0200

    implement curiosity model and create its route

---
+++ models/Curiosity.model.js
@@ -0,0 +1,30 @@
+const { Schema, model} = require("mongoose");
+
+const curiositySchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    date: Date,
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        enum: ["Natural Wonders", "Terrific Technology", "Curious Art", "Raving Reliques", "Interesting Stuff"],
+        default: "Interesting Stuff"
+    },
+    image: String,
+},
+
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model("Curiosity", curiositySchema);
