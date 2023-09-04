17678d0589d8fb3ee62be3d4004a56cda53128c7
Johanna Geßner <johanna-gessner@web.de>
2023-09-04 15:10:53 +0200

    delete curiosities / upload images

--- routes/curiosity.routes.js
+++ routes/curiosity.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Curiosity = require("../models/Curiosity.model");
 const User = require("../models/User.model");
+const fileUploader = require('../config/cloudinary.config');
 
 const isLoggedIn = require("../middleware/isLoggedIn");
 const isLoggedOut = require("../middleware/isLoggedOut");
@@ -10,83 +11,83 @@ const router = express.Router();
 
 //CREATE: display form 
 router.get("/curiosities-create", (req, res, next) => {
-   Curiosity.find()
+    Curiosity.find()
         .then((curiositiesFromDB) => {
-           res.render("curiosities/curiosity-create", {curiosities: curiositiesFromDB}); 
+            res.render("curiosities/curiosity-create", { curiosities: curiositiesFromDB });
         })
-            
-        .catch( (e) => {
+
+        .catch((e) => {
             console.log("Error getting list of curiosities from DB", e);
             next(e);
         });
 });
 
 router.get('/natural-wonders', (req, res, next) => {
-    Curiosity.find({category: "Natural Wonders"})
+    Curiosity.find({ category: "Natural Wonders" })
         .then((curiositiesFromDB) => {
-               res.render("curiosities/natural-wonders", {curiosities: curiositiesFromDB});   
+            res.render("curiosities/natural-wonders", { curiosities: curiositiesFromDB });
         })
-            
-        .catch( (e) => {
+
+        .catch((e) => {
             console.log("Error getting list of curiosities from DB", e);
             next(e);
         });
 });
 
 router.get('/terrific-technology', (req, res, next) => {
-    Curiosity.find({category: "Terrific Technology"})
-    .then((curiositiesFromDB) => {
-           res.render("curiosities/terrific-technology", {curiosities: curiositiesFromDB});   
-    })
-        
-    .catch( (e) => {
-        console.log("Error getting list of curiosities from DB", e);
-        next(e);
-    });
+    Curiosity.find({ category: "Terrific Technology" })
+        .then((curiositiesFromDB) => {
+            res.render("curiosities/terrific-technology", { curiosities: curiositiesFromDB });
+        })
+
+        .catch((e) => {
+            console.log("Error getting list of curiosities from DB", e);
+            next(e);
+        });
 })
 
 router.get('/curious-art', (req, res, next) => {
-    Curiosity.find({category: "Curious Art"})
-    .then((curiositiesFromDB) => {
-           res.render("curiosities/curious-art", {curiosities: curiositiesFromDB});   
-    })
-        
-    .catch( (e) => {
-        console.log("Error getting list of curiosities from DB", e);
-        next(e);
-    });
+    Curiosity.find({ category: "Curious Art" })
+        .then((curiositiesFromDB) => {
+            res.render("curiosities/curious-art", { curiosities: curiositiesFromDB });
+        })
+
+        .catch((e) => {
+            console.log("Error getting list of curiosities from DB", e);
+            next(e);
+        });
 })
 
 router.get('/raving-reliques', (req, res, next) => {
-    Curiosity.find({category: "Raving Reliques"})
-    .then((curiositiesFromDB) => {
-           res.render("curiosities/raving-reliques", {curiosities: curiositiesFromDB});   
-    })
-        
-    .catch( (e) => {
-        console.log("Error getting list of curiosities from DB", e);
-        next(e);
-    });
+    Curiosity.find({ category: "Raving Reliques" })
+        .then((curiositiesFromDB) => {
+            res.render("curiosities/raving-reliques", { curiosities: curiositiesFromDB });
+        })
+
+        .catch((e) => {
+            console.log("Error getting list of curiosities from DB", e);
+            next(e);
+        });
 })
 
 router.get('/fascinating-finds', (req, res, next) => {
-    Curiosity.find({category: "Fascinating Finds"})
-    .then((curiositiesFromDB) => {
-           res.render("curiosities/fascinating-finds", {curiosities: curiositiesFromDB});   
-    })
-        
-    .catch( (e) => {
-        console.log("Error getting list of curiosities from DB", e);
-        next(e);
-    });
+    Curiosity.find({ category: "Fascinating Finds" })
+        .then((curiositiesFromDB) => {
+            res.render("curiosities/fascinating-finds", { curiosities: curiositiesFromDB });
+        })
+
+        .catch((e) => {
+            console.log("Error getting list of curiosities from DB", e);
+            next(e);
+        });
 })
 
 
 
 //CREATE: process form
-router.post("/curiosities-create", (req, res, next) => {
+router.post("/curiosities-create", fileUploader.single('image'), (req, res, next) => {
 
-    const currentUser = req.user; 
+    const currentUser = req.user;
 
     const newCuriosity = {
         //user: currentUser._id,
@@ -94,7 +95,7 @@ router.post("/curiosities-create", (req, res, next) => {
         date: req.body.date,
         description: req.body.description,
         category: req.body.category,
-        image: req.body.image
+        image: req.file.path
     };
     console.log(req.body.description)
 
@@ -114,9 +115,6 @@ router.post("/curiosities-create", (req, res, next) => {
             } else {
                 res.redirect("/")
             }
-
-            
-            
         })
         .catch(e => {
             console.log("error creating a new curiosity", e);
@@ -124,7 +122,15 @@ router.post("/curiosities-create", (req, res, next) => {
         });
 });
 
+// DELETE: delete curiosities
+router.post('/:category/:id/delete', (req, res, next) => {
+    const id = req.params.id;
+    const category = req.params.category;
 
+    Curiosity.findByIdAndDelete(id)
+        .then(() => res.redirect(`/${category}`))
+        .catch(error => next(error));
+});
 
 
 module.exports = router;
