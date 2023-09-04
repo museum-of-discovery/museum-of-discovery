a9bf57e3f9c252ba4b4b2710089abb734975d3a2
Beatriz <beatrizrmb@outlook.com>
2023-09-04 10:16:05 +0100

    add curiosities category routes

--- routes/curiosity.routes.js
+++ routes/curiosity.routes.js
@@ -23,13 +23,35 @@ router.get("/curiosities-create", (req, res, next) => {
         });
 });
 
+router.get('/natural-wonders', (req, res, next) => {
+    res.render('curiosities/natural-wonders.hbs')
+});
+
+router.get('/terrific-technology', (req, res, next) => {
+    res.render('curiosities/terrific-technology.hbs')
+})
+
+router.get('/curious-art', (req, res, next) => {
+    res.render('curiosities/curious-art.hbs')
+})
+
+router.get('/raving-reliques', (req, res, next) => {
+    res.render('curiosities/raving-reliques.hbs')
+})
+
+router.get('/fascinating-finds', (req, res, next) => {
+    res.render('curiosities/fascinating-finds.hbs')
+})
+
+
+
 //CREATE: process form
 router.post("/curiosities-create", (req, res, next) => {
 
     const currentUser = req.user; 
 
     const newCuriosity = {
-        user: currentUser._id,
+        //user: currentUser._id,
         title: req.body.title,
         date: req.body.date,
         description: req.body.description,
@@ -40,13 +62,30 @@ router.post("/curiosities-create", (req, res, next) => {
 
     Curiosity.create(newCuriosity)
         .then((newCuriosity) => {
-            res.redirect("/")
+            if (req.body.category === "Natural Wonders") {
+                res.redirect("/natural-wonders")
+            } else if (req.body.category === "Terrific Technology") {
+                res.redirect("/terrific-technology")
+            } else if (req.body.category === "Curious Art") {
+                res.redirect("/curious-art")
+            }
+            else if (req.body.category === "Raving Reliques") {
+                res.redirect("/raving-reliques")
+            } else if (req.body.category === "Fascinating Finds") {
+                res.redirect("/fascinating-finds")
+            } else {
+                res.redirect("/")
+            }
+
+            
+            
         })
         .catch(e => {
             console.log("error creating a new curiosity", e);
             next(e);
         });
 });
+
 
 
 
